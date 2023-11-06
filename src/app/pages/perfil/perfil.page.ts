420b6dcdebc953b6ced248da535f228a351eb8d2
Aldoarroyoo <ald.arroyo@duocuc.cl>
2023-11-06 11:08:09 -0300

    storage

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Storage } from '@ionic/storage-angular';
+
 
 @Component({
   selector: 'app-perfil',
@@ -9,22 +11,27 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 })
 export class PerfilPage implements OnInit {
   user:any
-  constructor(private router: Router ,private authenticationService: AuthenticationService) {
+  constructor(private router: Router ,private authenticationService: AuthenticationService, private storage: Storage) {
     this.user = authenticationService.getProfile();
   }
 
   ngOnInit() {
-
+    
   }
 
 
 
   async logout(){
     this.authenticationService.signOut().then(()=>{
+      this.removeUserData()
       this.router.navigate(['login']).catch((error)=>{
         console.log(error);
       })
     })
+  }
+
+  async removeUserData() {
+    await this.storage.remove('user');
   }
 
 
