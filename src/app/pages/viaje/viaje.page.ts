f5549c13e681ecede16f1b037d3a6c612464c109
Aldo Arroyo <ald.arroyo@duocuc.cl>
2023-11-26 17:01:36 -0300

    respaldo

--- src/app/pages/viaje/viaje.page.ts
+++ src/app/pages/viaje/viaje.page.ts
@@ -30,6 +30,13 @@ export class ViajePage implements OnInit {
       });
     }
 
+    async abrirModal() {
+      const modal = document.querySelector('ion-modal');
+        if (modal) {
+          modal.present();
+        };
+    }
+
   async ngOnInit() {
     this.firestoreService.getViajePorId(this.viajeid).subscribe((viaje: any) => {
       // Aquí puedes utilizar el objeto del viaje obtenido por su ID
@@ -107,24 +114,6 @@ export class ViajePage implements OnInit {
     }
   }
   
-
-  
-
-
-  //Como se extraen estos datos del storage a pesar de que la vista valida de que si esta el elemento, no alcanza a abrir
-  //el modal con los datos, por lo que despues de que carguen la información del storage se fuerza la visualización del 
-  //modal con los datos del viaje
-  ngAfterViewInit() {
-    this.route.queryParams.subscribe((params) => {
-      const modal = document.querySelector('ion-modal');
-      if (modal) {
-        modal.componentProps = {
-          viaje: this.viaje,
-        };
-        modal.present();
-      }
-    });
-  }
 
   async presentAlert(message: string) {
     const alert = await this.alertController.create({
